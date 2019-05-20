22ce48f8ef52c94498df97b4df9e0c53f7e95953
aadeliafitri <42019804+aadeliafitri@users.noreply.github.com>
2019-05-20 11:19:31 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,5 @@
+<?php
+ echo "Namaku Adel";
+ echo "<br>";
+ echo "Aku sedang belajar PHP di SMK Telkom Malang";
+ ?>
---
+++ latihan3_3.php
@@ -0,0 +1,10 @@
+<?php
+  $user = "Adelll";
+  $pass = "123";
+  if ($user == "Adelll" && $pass == "123") {
+    echo "Login Berhasil";
+  }
+  else {
+    echo "Login Gagal";
+  }
+ ?>
